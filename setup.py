ac967473d64fd5a9bb7a9d1b410d48bf9eaa9674
Stefano Crosta <stefano.crosta@gmail.com>
2013-07-02 04:57:00 +0200

    Work with custom user models in django >= 1.5

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(
     name='django-hierarchical-auth',
-    version='0.1.dev',
-    author='Ivan Raskovsky (rasca)',
+    version='0.3.dev',
+    author='Ivan Raskovsky (rasca), Stefano Crosta (scrosta)',
     author_email='raskovsky@gmail.com',
     packages=['hierarchical_auth',],
     # license='Creative Commons Attribution-Noncommercial-Share Alike license',
